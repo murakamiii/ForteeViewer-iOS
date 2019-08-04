fdaa335e4a0d37f1ee9c864fc8b3c2feb255c9ce
murakamitaichi <9khc727ru7@gmail.com>
2019-08-05 00:58:20 +0900

    Initial Commit

---
+++ HalfModalyFortee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HalfModalyFortee
+//
+//  Created by murakami Taichi on 2019/08/05.
+//  Copyright © 2019 murakammm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
